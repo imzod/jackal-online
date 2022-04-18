26e52705f5425a1dbfabf58c94d4b25600476738
imzod <imzod@yandex.ru>
2022-04-18 22:03:20 +0500

    first commit

---
+++ app.js
@@ -0,0 +1,37 @@
+const express = require('express');
+const ejsMate = require('ejs-mate');
+const path = require("path");
+const ExpressError = require("./utils/ExpressError");
+const bodyParser = require("body-parser");
+
+
+const app = express();
+
+app.engine('ejs', ejsMate);
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'))
+
+app.use(bodyParser.json());
+app.use(express.urlencoded({extended: true}));
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.get('/', (req, res) => {
+    res.render('game');
+});
+
+
+
+app.all('*', (req, res, next) => {
+    next(new ExpressError('Page not found', 404))
+});
+
+/*app.use((err, req, res, next) => {
+    const {statusCode = 500} = err;
+    if (!err.message) err.message = 'Oh no, ERROR!!';
+    res.status(statusCode).render('error', {err});
+})*/
+
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
+    console.log(`Serving on port ${port}`)
+})
